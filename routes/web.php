ffea76c99bb6a9c62732ce196b424b245a8d4052
Agung Nugroho <nugroho.nunukkedung056@gmail.com>
2025-11-23 04:46:01 +0700

    update tampilan form leasing

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,25 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/dashboard', function () {
+    return view('dashboard');
+});
+
+Route::get('/profil', function () {
+    return view('profil');
+});
+
+Route::get('/ajukankontrak', function () {
+    return view('ajukankontrak');
+});
+
+Route::get('/pelanggan', function () {
+    return view('dftrpelanggan');
+});
+
+Route::get('/pengingat', function () {
+    return view('pengigatbyr');
+});
+Route::get('/ajuansaya', function () {
+    return view('ajuansaya');
+});
